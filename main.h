1e22e6fdb61d4fb9a341690b586939f36be021ae
01Fab <125474498+01Fab@users.noreply.github.com>
2023-04-25 13:08:11 +0200

    Update

--- main.h
+++ main.h
@@ -98,7 +98,7 @@ int print_percent(va_list types, char buffer[],
         int flags, int width, int precision, int size);
 
 
-/* Functions to print numbers */
+/* Functions to print any numbers */
 
 int print_int(va_list types, char buffer[],
 
